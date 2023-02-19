e5b2ce3014f3a9f0cd42cd0c96318cf525f9a233
AnnaKiryukhina <124432443+AnnaKiryukhina@users.noreply.github.com>
2023-02-19 17:14:35 +0700

    Seminar 003 решение

---
+++ задача 1/Program.cs
@@ -0,0 +1,30 @@
+﻿ int[] coords = new int[2];
+ System.Console.Write("Введите X: ");
+ coords[0] = Convert.ToInt32(Console.ReadLine());
+
+// Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+System.Console.Write("\nВведите Y: ");
+coords[1] = Convert.ToInt32(Console.ReadLine());
+
+if (coords[0] > 0 && coords[1] > 0)
+{
+    System.Console.WriteLine("Мы находимся в первой четверти");
+}
+else if (coords[0] < 0 && coords[1] > 0)
+{
+    System.Console.WriteLine("Мы находимся во второй четверти");
+}
+else if (coords[0] < 0 && coords[1] < 0)
+{
+    System.Console.WriteLine("Мы находимся в третьей четверти");
+}
+else if (coords[0] > 0 && coords[1] < 0)
+{
+    System.Console.WriteLine("Мы находимся в четвертой четверти");
+}
+else
+{
+    System.Console.WriteLine("Координата лежит на осях");
+}
