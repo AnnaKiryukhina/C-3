885477ad629df3f81218ae056102aef9e006204d
AnnaKiryukhina <124432443+AnnaKiryukhina@users.noreply.github.com>
2023-02-20 13:41:56 +0700

    Seminar 3 ДЗ задача 19  решение

---
+++ ДЗ_Задача_19/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите число: ");
+string? number = Console.ReadLine();
+
+if (number [0] == number[4] || number[1] == number [3])
+{
+    System.Console.WriteLine("Полиндром");
+}
+else
+{
+    System.Console.WriteLine("не является полиндромом");
+}
+
+
+
+
+// int Result(int A)
+// {
+//     int counter = Convert.ToString(A).Length;
+//     int sum = 0;
+  
+//     for ( int i = 0; i < counter; i++)
+//     {
+//         sum = sum + A % 10;
+//         A = A / 10;
+//     }
+
+//     return sum;
+
+// }
+// int number = ReadInt("Введите число А: ");
+// System.Console.WriteLine($"Сумма цифр в числе {number} = { Result (number)}");
